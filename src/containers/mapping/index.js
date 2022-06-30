f2882c592aafe8fd2b4b70002fdd69634cce045a
Patrick de Campos Nascimento <patrickcampos77@hotmail.com>
2022-06-29 21:58:33 -0300

    criacao lista de hemocentros

--- src/containers/mapping/index.js
+++ src/containers/mapping/index.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-import-assign */
 import React from "react";
 import GoogleMapReact from "google-map-react";
+
+const hemocentros = require("../../assets/json/hemocentros.json");
 
 export default function Mapping() {
   const defaultProps = {
@@ -9,6 +12,20 @@ export default function Mapping() {
     },
     zoom: 11,
   };
+  const lista = hemocentros.list;
+  console.log(lista[0]);
+  const listItem = (name, local, telefone, key) => {
+    return (
+      <div key={key + 1} style={{
+        p : 10,
+        m : 10
+      }}>
+        <h3>{name}</h3>
+        <label>{local}</label>
+        <h3>{telefone}</h3>
+      </div>
+    );
+  };
 
   return (
     <>
@@ -16,10 +33,36 @@ export default function Mapping() {
         <GoogleMapReact
           bootstrapURLKeys={{
             key: "AIzaSyBchQuQlmxRk - LeSQbgJ_cRibxNLKtovak",
+            language: "pt-br",
+            region: "pt-br",
           }}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
         ></GoogleMapReact>
+      </div>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <h1>Hemocentros</h1>
+        </div>
+        <div>
+          {lista.map((item, index) => {
+            //         listItem(item.name, item.local,item.telefone);
+            return (
+              <>
+                  {listItem(item.name, item.local, item.telefone, index)}
+                  <hr key = {index * 3}/>
+              </>
+
+              // <ItemList key= {index} name = {item.name} local = {item.local} telefone = {item.telefone}/>
+            );
+          })}
+        </div>
       </div>
     </>
   );
